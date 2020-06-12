2bef4559ba82d3e9c27f04156438411d7aeb6d18
Kevin Fitzgerald <kevin@kevinfitzgerald.net>
2020-06-12 16:17:20 -0500

    Added farm vendor sync logs routes

--- dist/client.js
+++ dist/client.js
@@ -82,7 +82,7 @@ function Client(config) {
 /**
  * SDK Version
  */
-Client.Version = '3.8.0';
+Client.Version = '3.9.0';
 
 /**
  * Expose the Provider base class
@@ -2729,6 +2729,29 @@ Client.resourceBinders.push(function(Client) {
                 path: '/api/reporting/links/top-n',
                 query: query
             }, callback);
+        },
+        
+        /**
+         * Returns a histogram of vendor sync metrics for time-series visualizations
+         * @param {object} [query] - Filter arguments
+         * @param {requestCallback} [callback] – Optional callback. When present, the request is executed
+         * @return {Query} - Compiled query ready for execution
+         * @memberof Client.reportings#
+         */
+        vendor_sync_histogram: function(query, callback) {
+            // Shift optional arguments, if necessary
+            if (typeof query === "function") {
+                callback = query;
+                query = undefined;
+            }
+    
+            return Client._makeRequest({
+                api: 'farm',
+                action: 'reporting.vendor_sync_histogram',
+                method: 'GET',
+                path: '/api/reporting/vendor-syncs/date-histogram',
+                query: query
+            }, callback);
         }
         
     };
@@ -2828,6 +2851,62 @@ Client.resourceBinders.push(function(Client) {
                     instance_id: instance_id,
                     vendor_id: vendor_id
                 }
+            }, callback);
+        }
+        
+    };
+    
+    /**
+     * Vendors
+     * @namespace Client.farm.vendor_syncs
+     */
+    Client.farm.vendor_syncs = {
+        
+        /**
+         * Returns detailed information about a vendor sync.
+         * @param {string} instance_id – Instance Id
+         * @param {string} sync_id – Vendor Sync Id
+         * @param {requestCallback} [callback] – Optional callback. When present, the request is executed
+         * @return {Query} - Compiled query ready for execution
+         * @memberof Client.vendor_syncs#
+         */
+        retrieve: function(instance_id, sync_id, callback) {
+            return Client._makeRequest({
+                api: 'farm',
+                action: 'vendor_sync.retrieve',
+                method: 'GET',
+                path: '/api/{instance_id}/vendors/syncs/{sync_id}',
+                pathParams: {
+                    instance_id: instance_id,
+                    sync_id: sync_id
+                }
+            }, callback);
+        },
+        
+        /**
+         * Returns a list of vendor sync logs.
+         * @param {string} instance_id – Instance Id
+         * @param {object} [query] - Filter arguments
+         * @param {requestCallback} [callback] – Optional callback. When present, the request is executed
+         * @return {Query} - Compiled query ready for execution
+         * @memberof Client.vendor_syncs#
+         */
+        list: function(instance_id, query, callback) {
+            // Shift optional arguments, if necessary
+            if (typeof query === "function") {
+                callback = query;
+                query = undefined;
+            }
+    
+            return Client._makeRequest({
+                api: 'farm',
+                action: 'vendor_sync.list',
+                method: 'GET',
+                path: '/api/{instance_id}/vendors/syncs',
+                pathParams: {
+                    instance_id: instance_id
+                },
+                query: query
             }, callback);
         }
         
